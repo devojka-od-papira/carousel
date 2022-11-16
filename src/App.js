3c0381dfa3e86a2ca3388a46a7811685e4ed397b
Bojana Bojic <bojic@work.co>
2022-11-16 09:23:15 +0100

    Create carousel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
+
+import "./index.css";
 
 function App() {
+  const colors= ['red', 'blue', 'green', 'yellow', 'purple', 'black', 'salmon'];
+  const [position, setPosition] = useState(0);
+  let carousel = document.querySelector(".carousel");
+
+  const next = () => {
+    console.log('next')
+    if (position <= 1) {
+      setPosition(position + 1)
+      carousel.style.transition = 'all 0.75s ease-in-out';
+    }
+  }
+
+  const preview = () => {
+    console.log('preview')
+    if(position >= 1){
+      setPosition(position - 1)
+    }
+  }
+
+  console.log("position", position)
   return (
     <div>
-    Carusel
+      <div className='carousel'>
+        <div className={`box ${colors[position]}`}></div>
+        <div className={`box ${colors[position + 1]}`}></div>
+        <div className={`box ${colors[position + 2]}`}></div>
+        <div className={`box ${colors[position + 3]}`}></div>
+        <div className={`box ${colors[position + 4]}`}></div>
+      </div>
+      <div className='carousel-control-wrapper'>
+        <button onClick={preview} className='carousel-control'>{'<'}</button>
+        <button onClick={next} className='carousel-control'>{'>'}</button>
+      </div>
     </div>
   );
 }
